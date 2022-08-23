4845b0aadec4150d4dd5fc75422c0c718e30a425
LeoAntunesBrombilla <antunes.b.leonardo@gmail.com>
2022-08-23 08:09:00 -0300

    adding basic widgets

--- lib/main.dart
+++ lib/main.dart
@@ -7,16 +7,38 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      color: Colors.green,
-      home: Scaffold(
-        body: Center(
-          child: Image.network('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
-        ) ,
-      )
-    );
+        color: Colors.green,
+        home: Scaffold(
+          body: Center(
+            child: Column(mainAxisSize: MainAxisSize.min, children: [
+              Text('Texto'),
+              SizedBox(height: 8),
+              Icon(Icons.alarm),
+              Text('Texto 2'),
+              SizedBox(
+                  height: 100,
+                  width: 100,
+                  child: Container(color: Colors.green)),
+              Row(mainAxisSize: MainAxisSize.min, children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                      onPressed: () {
+                        print('Clique');
+                      },
+                      child: Text('Clique')),
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      print('Clique');
+                    },
+                    child: Text('Clique'))
+              ])
+            ]),
+          ),
+        ));
   }
 }
